d2519ec057419f27748ffd89d83b9ed68f729a5f
SebastianMH14 <sebasmh2002@hotmail.com>
2022-05-05 11:26:43 -0500

    Add create users

---
+++ controllers/UsersController.js
@@ -0,0 +1,29 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+export default class Users {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    console.log(email, password);
+
+    if (!email) {
+      return res.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      return res.status(400).json({ error: 'Missing password' });
+    }
+    const user = await dbClient.client
+      .db('files_manager')
+      .collection('users')
+      .findOne({ email });
+    if (user) {
+      return res.status(400).json({ error: 'Already exist' });
+    }
+    const hash = sha1(password);
+    const newUser = await dbClient.client
+      .db('files_manager')
+      .collection('users')
+      .insert({ email, password: hash });
+    return res.status(201).json({ id: newUser._id, email: newUser.email });
+  }
+}
